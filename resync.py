b1fe09569279edf1e72e6708035982e53f2aacba
Chris Angelico <rosuav@gmail.com>
2019-06-25 22:21:34 +1000

    Go back to using frame numbers as there may be a simplification possible

--- resync.py
+++ resync.py
@@ -7,10 +7,12 @@ import subprocess
 # Ideally, this should span a hard cut in the movie, which makes frame
 # counting easy. NOTE: This may take a couple of minutes of chugging,
 # and may not show much output on the console while it does it.
-frames = { # Keep these in order of frame number
-	2839: "find_console", # When Joy finds the console for the first time
-	158682: "cat_yelp", # The cat from the end credits - from console to yelp
-}
+frames = [
+	2839, # When Joy finds the console for the first time
+	158682, # The cat from the end credits - from console to yelp
+]
+
+frames.sort() # The renaming at the end depends on these being in order.
 
 expr = "+".join("eq(n\\,%d)+eq(n\\,%d)" % (frm, frm+1) for frm in frames)
 tmpfn = "sync/tmp%d.png"
@@ -20,5 +22,10 @@ subprocess.check_call([
 	"-vframes", str(len(expr)), "-s", "640x180",
 	"-vsync", "0", tmpfn,
 ])
-for idx, name in enumerate(frames.values(), 1):
-	os.rename(tmpfn % idx, "sync/%s.png" % name)
+for idx, frm in enumerate(frames, 1):
+	os.rename(tmpfn % idx, "sync/%d.png" % frm)
+
+# TODO: Once ffmpeg builds that include the frame_pts parameter become more
+# common, ditch the rename step at the end in favour of "-frame_pts","1".
+# That may also remove the need to sort the frame list, although that's not
+# expensive.
