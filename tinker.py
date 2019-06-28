9ca72c07bd67ed819c9d08b32fc285f15be06bee
Chris Angelico <rosuav@gmail.com>
2019-06-28 20:12:54 +1000

    Begin speccing up some goals

---
+++ tinker.py
@@ -0,0 +1,15 @@
+"""
+Goals
+-----
+
+1) Lift images out of movie - pillow? if not, ffmpeg
+2) Crop specific portions from specific frames
+3) Synthesize sub-pieces from text
+3a) Narration - square box
+3b) Speech bubble incl pointer
+3c) Variants eg "boom" sounds, dice rolls?
+4) Combine sub-pieces to assemble a coherent page
+5) Govern everything with a simple text file
+
+"""
+import PIL.Image
